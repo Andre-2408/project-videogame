9616cee77dbc3f811c8dda1a12389b7b2031f26d
YuckierOlive370 <urbinatrujillocesarmanuel7@gmail.com>
2026-03-22 14:48:17 -0700

    2 niveles nuevos, fix en colisiones

--- Project-Videogame/Assets/Scripts/KillZone.cs
+++ Project-Videogame/Assets/Scripts/KillZone.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 /// <summary>
 /// Coloca este script en un GameObject con Collider2D (Is Trigger = true)
@@ -9,6 +10,9 @@ public class KillZone : MonoBehaviour
 {
     [SerializeField] private Transform spawnPoint;  // arrastrar el SpawnPoint del jugador aquí
     [SerializeField] private int       damage = 1;  // daño por caer al vacío
+    [Header("Nombre de la escena a cargar")]
+    [Tooltip("Debe coincidir con el nombre en Build Settings")]
+    public string sceneToLoad;
 
     private void OnTriggerEnter2D(Collider2D col)
     {
@@ -27,6 +31,17 @@ public class KillZone : MonoBehaviour
             // Resetear velocidad para que no siga con el impulso de la caída
             var rb = col.GetComponent<Rigidbody2D>();
             if (rb != null) rb.linearVelocity = Vector2.zero;
+        }
+        else
+        {
+            if (!string.IsNullOrEmpty(sceneToLoad))
+            {
+                SceneManager.LoadScene(sceneToLoad);
+            }
+            else
+            {
+                Debug.LogWarning("No se ha asignado ninguna escena en el Inspector.");
+            }
         }
     }
 }
